26e1c72674c33c7b8352daa793dd0712ecc28770
AkShAy21211 <akshay68580@gmail.com>
2024-07-16 19:53:53 +0530

    google auth fix

--- src/infrastructure/config/passport.ts
+++ src/infrastructure/config/passport.ts
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "/user/auth/google/callback",
       passReqToCallback: true,
       
     },
