fe0f3e67dfb22d32a384186c109307da5fe476ec
AkShAy21211 <akshay68580@gmail.com>
2025-01-03 11:56:29 +0530

    fix/ socket.io congiguration

--- src/infrastructure/config/db.ts
+++ src/infrastructure/config/db.ts
@@ -2,12 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    
-    const mongoUri = process.env.NODE_ENV==='DEVELOPMENT'?process.env.MONGO_DEV_URI:process.env.MONGO_URI as string;
-    await mongoose.connect(mongoUri as string);
-    console.log('Connected to MongoDB');
+    const mongoUri =
+      process.env.NODE_ENV === "DEVELOPMENT"
+        ? process.env.MONGO_DEV_URI
+        : (process.env.MONGO_URI as string);
+    await mongoose.connect(mongoUri as string, {
+      serverSelectionTimeoutMS: 50000, // Increase timeout to 50 seconds
+      socketTimeoutMS: 45000, // Adjust socket timeout
+    });
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.log('error connection db',error);
+    console.log("error connection db", error);
   }
 };
 
